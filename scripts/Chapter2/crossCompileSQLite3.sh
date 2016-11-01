fe599949be94ef70a32726d1f9baf5363d6aa3a8
Bilel Eljaamii <bilel.eljaamii@gmail.com>
2016-11-01 03:14:05 +0100

    Finishing Chapter 2 -ToolChains- Summary

--- scripts/Chapter2/crossCompileSQLite3.sh
+++ scripts/Chapter2/crossCompileSQLite3.sh
@@ -10,5 +10,9 @@ cd sqlite-autoconf-3150000
 
 
 CC=~/x-tools/arm-cortex_a8-linux-gnueabihf/bin/arm-cortex_a8-linux-gnueabihf-gcc ./configure --host=arm-cortex_a8-linux-gnueabihf 
-
 make 
+
+echo "\t \t \t Installing SQLite  ... "
+make DESTDIR=$(~/x-tools/arm-cortex_a8-linux-gnueabihf/bin/arm-cortex_a8-linux-gnueabihf-gcc -print-sysroot) install > /dev/null 2>&1 && echo "\t \t \t SQLite  is successfully installed ... "
+
+tree ~/x-tools/arm-cortex_a8-linux-gnueabihf/arm-cortex_a8-linux-gnueabihf/sysroot/usr/local/
